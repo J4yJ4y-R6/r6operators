fa55ea0bbde3bb6a75eedaa5e1cc82ef77431ce9
Jannik Junker <jannik.junker2018@t-online.de>
2026-03-03 01:12:02 +0100

    feat: add solid snake

---
+++ operators/solid_snake/index.ts
@@ -0,0 +1,24 @@
+import { IOperator } from "~/types/operator"
+
+export const solid_snake: IOperator = {
+  name: "Solid Snake",
+  role: "Attacker",
+  org: "",
+  squad: "",
+  ratings: {
+    health: 1,
+    speed: 3,
+    difficulty: 2,
+  },
+  meta: {
+    gender: "m",
+    country: "us",
+    season: "Y11S1",
+    height: 0,
+    weight: 0,
+  },
+  bio: {
+    real_name: "[REDACTED]",
+    birthplace: "[REDACTED]",
+  },
+}
