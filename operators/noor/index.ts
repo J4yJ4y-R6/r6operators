ed787f9bae81273f0ff7fec373e5000a014aa083
Jannik Junker <jannik.junker2018@t-online.de>
2026-09-01 01:35:12 +0200

    feat: add new operator noor

---
+++ operators/noor/index.ts
@@ -0,0 +1,24 @@
+import { IOperator } from "~/types/operator"
+
+export const noor: IOperator = {
+  name: "Noor",
+  role: "Defender",
+  org: "DELTA FORCE",
+  squad: "",
+  ratings: {
+    health: 2,
+    speed: 2,
+    difficulty: 2,
+  },
+  meta: {
+    gender: "m",
+    country: "eg",
+    season: "Y11S3",
+    height: 181,
+    weight: 79,
+  },
+  bio: {
+    real_name: "Ahmed Murad",
+    birthplace: "Ben Suef, Egypt",
+  },
+}
